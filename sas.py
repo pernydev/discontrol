a34206ac67149728cf65878e2f601a198ba8c396
pernydev <83672513+pernydev@users.noreply.github.com>
2022-12-29 11:56:31 +0200

    Forgot to exit the function

--- sas.py
+++ sas.py
@@ -1,16 +1,15 @@
 import tkinter as tk
 
-def confirmation():
-    popup = tk.Tk()
-    popup.wm_title("WARNING!")
+popup = tk.Tk()
+popup.wm_title("WARNING!")
 
-    label = tk.Label(popup, text="The file you just opened is a RAT virus designed for educational purposes.")
-    label.pack(side="top", fill="x", pady=10)
+label = tk.Label(popup, text="The file you just opened is a RAT virus designed for educational purposes.")
+label.pack(side="top", fill="x", pady=10)
 
-    cancel_button = tk.Button(popup, text="Cancel", command=popup.destroy)
-    cancel_button.pack()
-    
-    continue_button = tk.Button(popup, text="Continue (Giving access to your computer)", command=lambda: run(popup))
-    continue_button.pack()
+cancel_button = tk.Button(popup, text="Cancel", command=popup.destroy)
+cancel_button.pack()
 
-    popup.mainloop()
+continue_button = tk.Button(popup, text="Continue (Giving access to your computer)", command=lambda: run(popup))
+continue_button.pack()
+
+popup.mainloop()
